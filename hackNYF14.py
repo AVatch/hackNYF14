2d7c324e714a4f31a49912ff3e1f41a2ffd1145e
AVatch <akv224@nyu.edu>
2014-10-18 20:38:33 -0400

    post handler

--- hackNYF14.py
+++ hackNYF14.py
@@ -10,9 +10,31 @@ db = pymongo.MongoClient('localhost', 27017)['hackNY']
 
 
 class MainHandler(tornado.web.RequestHandler):
+    def options(self):
+        self.set_status(200)
+
+    def set_default_headers(self):
+        self.set_header('Access-Control-Allow-Origin', '*')
+        self.set_header('Access-Control-Allow-Methods',
+                        'POST, GET, PUT, DELETE, OPTIONS')
+        self.set_header('Access-Control-Allow-Credentials', False)
+        self.set_header('Access-Control-Allow-Headers',
+                        'X-Requested-With, X-HTTP-Method-Override, \
+                        Content-Type, Accept')
+
     def get(self):
         response = {}
         response["message"] = "Yo"
+        self.write(json.dumps(response))
+
+    def post(self):
+        response = {}
+        response['reason'] = {}
+
+        request = json.loads(self.request.body)
+
+        response['content'] = request
+
         self.write(json.dumps(response))
 
 
