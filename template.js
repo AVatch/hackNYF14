9f9c6b09ceef48b1b642c97da62d7e44a527d30f
adriansoghoian <adriansoghoian@gmail.com>
2014-10-19 01:03:26 -0400

    Added CSS and JS files

---
+++ template.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	var focus_level = 0;
+	var artwork = [];
+	var artwork_index = 0;
+	var user_id = 0;
+
+	$(#prompt).submit(function() {
+		user_id = $(#user_id).val();
+		console.log(user_id);
+	})
+
+	// function refresh_focus_level() {
+
+	// }
+
+})
